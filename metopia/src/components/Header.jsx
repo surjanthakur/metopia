4e520f933407edf64d9e6c752801fb21e10157b9
Surjanthakur <tsurjan506@gmail.com>
2025-11-29 00:17:17 +0530

    save

--- metopia/src/components/Header.jsx
+++ metopia/src/components/Header.jsx
@@ -1,3 +1,31 @@
+import { NavLink } from 'react-router-dom'
+import './header.css'
 export default function Header() {
-  return <div></div>
+  return (
+    <>
+      <header className="sticky top-0 left-0 z-50">
+        <nav className="w-20 flex-row  justify-evenly">
+          <h1 className="brand-name">Metopia</h1>
+          <div>
+            <NavLink className="creator text-white" to="/creators">
+              for Creators
+            </NavLink>
+            <NavLink className="brands text-white" to="/brands">
+              for Brands
+            </NavLink>
+            <NavLink className="blog text-white" to="/blog">
+              Blog
+            </NavLink>
+            <NavLink className="about text-white" to="/about">
+              About
+            </NavLink>
+          </div>
+          <div>
+            <button>Log in </button>
+            <button>Book a Demo</button>
+          </div>
+        </nav>
+      </header>
+    </>
+  )
 }
